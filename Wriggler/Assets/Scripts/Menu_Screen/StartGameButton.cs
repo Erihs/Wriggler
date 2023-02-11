3815e6910f77e5277f82974b0f6a21b562c1b1bf
Erihs <115496133+Erihs@users.noreply.github.com>
2023-02-11 21:24:53 +0200

    Menu_Start_Work

--- Wriggler/Assets/Scripts/Menu_Screen/StartGameButton.cs
+++ Wriggler/Assets/Scripts/Menu_Screen/StartGameButton.cs
@@ -1,15 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGameButton : MonoBehaviour
 {
-/*
     public int gameStartScene;
 
-    private void StartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene(gameStartScene);
     }
-*/
 }

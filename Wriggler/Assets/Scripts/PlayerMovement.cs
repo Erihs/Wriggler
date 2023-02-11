bc1782aab17030b564718e1427d14e4b3b5b5fef
P6antom <pascalkaiser55@gmail.com>
2023-02-11 15:43:25 +0200

    Added damage enemy
    with 2Dcollider

--- Wriggler/Assets/Scripts/PlayerMovement.cs
+++ Wriggler/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float speed = 4f;
+    private float jumpingPower = 8f;
     private bool isFacingRight = true;
     private float horizontalInput;
 
--- Wriggler/Assets/Scripts/PlayerRespawn.cs
+++ Wriggler/Assets/Scripts/PlayerRespawn.cs
@@ -24,7 +24,7 @@ public class PlayerRespawn : MonoBehaviour
             currentCheckpoint = collision.transform;
             collision.GetComponent<Collider2D>().enabled = false;
 
-            //Debug.Log("respawn");
+            Debug.Log("respawn");
         }
     }
 }

64aff752c8f3110eab697d0a12d3e9b000f3427a
Erihs <115496133+Erihs@users.noreply.github.com>
2023-05-29 23:38:50 +0300

    Button sounds

---
+++ Wriggler/Assets/Scripts/Menu_Screen/QuitButton.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public AudioSource buttoclick;
+
+    public void PlayButtonSound()
+    {
+        buttoclick.Play();
+    }
+
+    public void QuitGame () 
+    {
+        Application.Quit ();
+        Debug.Log("Game is exiting");
+        //Just to make sure its working
+    }
+}
